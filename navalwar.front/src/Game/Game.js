00931447b026a6ed3f0270de722af77580caf34c
Solène TESSIORE <solene.tessiore@etu.uca.fr>
2023-02-09 17:26:16 +0100

    Mise en place des routes pour la page de login et la page de jeu

---
+++ navalwar.front/src/Game/Game.js
@@ -0,0 +1,7 @@
+import './Game.css';
+
+export const Game = () => {
+    return (
+        <h1>Game page</h1>
+    )
+}
---
+++ navalwar.front/src/Login/Login.js
@@ -0,0 +1,7 @@
+import './Login.css';
+
+export const Login = () => {
+    return (
+        <h1>Login page</h1>
+    )
+}
