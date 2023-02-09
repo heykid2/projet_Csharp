a40cf0a0d77c8005bb5042367c425a8403266f29
Solène TESSIORE <solene.tessiore@etu.uca.fr>
2023-02-09 17:36:01 +0100

    Ajout d'un squelette de formulaire sur la page de connexion

--- navalwar.front/src/Login/Login.js
+++ navalwar.front/src/Login/Login.js
@@ -1,7 +1,20 @@
 import './Login.css';
+import { useContext, useState } from 'react';
 
 export const Login = () => {
+
+    const [pseudo, setpseudo] = useState("");
+
     return (
-        <h1>Login page</h1>
+        <form action="" method="post" onSubmit={(e) => mySubmit(e)}>
+            <label><strong>Commencer une partie</strong></label>
+            <div>
+                <label>Pseudo : </label>
+                <input type="text" name="name" id="name" placeholder="Tabouret" required onChange={(e) => setpseudo(e.target.value)} />
+            </div>
+            <div>
+                <input type="submit" value="C'est parti !" required />
+            </div>
+        </form>
     )
 }
